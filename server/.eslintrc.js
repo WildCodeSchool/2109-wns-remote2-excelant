447d5293e96749f45cef45ef8fdedaf36e4a9bff
Lionel Delamare <ldelamare@entreloups.com>
2021-10-29 10:39:45 +0200

    eslint server side initialization

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -1,13 +1,13 @@
 module.exports = {
     env: {
-        commonjs: true,
+        browser: true,
         node: true,
         es2021: true
     },
     extends: ['airbnb-base', 'airbnb-typescript/base', 'prettier'],
     parser: '@typescript-eslint/parser',
     parserOptions: {
-        ecmaVersion: 2021,
+        ecmaVersion: 12,
         sourceType: 'module',
         project: './tsconfig.json',
         tsconfigRootDir: './'
--- server/server.ts
+++ server/server.ts
@@ -1,4 +1,4 @@
-const { ApolloServer, gql } = require('apollo-server');
+import { ApolloServer, gql } from 'apollo-server';
 
 // The GraphQL schema
 const typeDefs = gql`
