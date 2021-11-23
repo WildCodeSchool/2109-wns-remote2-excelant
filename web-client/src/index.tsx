e2e837a579aa9804919b61459c498e77478f42e7
wild-e <62098185+wild-e@users.noreply.github.com>
2021-11-23 14:53:47 +0100

    done for installing graphQL on client side + testing with hello query (#16)
    * done for installing graphQL on client side + testing with hello query
    
    * correction. Added a ternary, and handling case when no data

--- web-client/src/index.tsx
+++ web-client/src/index.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+} from "@apollo/client";
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+  cache: new InMemoryCache()
+});
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ApolloProvider client={client}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  </ApolloProvider>,
   document.getElementById('root')
 );
 
