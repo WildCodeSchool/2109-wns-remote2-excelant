d7bd29d69ec1749ae4f975770e4838376d4fdfd0
wild-e <62098185+wild-e@users.noreply.github.com>
2021-11-23 14:53:47 +0100

    done for installing graphQL on client side + testing with hello query (#16)
    * done for installing graphQL on client side + testing with hello query
    
    * correction. Added a ternary, and handling case when no data

--- web-client/src/App.tsx
+++ web-client/src/App.tsx
@@ -1,16 +1,27 @@
-import React from "react";
+import React from 'react';
 import { ReactJSXElement } from "@emotion/react/types/jsx-namespace";
-import logo from "./logo.svg";
+import { gql, useQuery } from '@apollo/client';
+import logo from './logo.svg';
 import "./App.scss";
 import "./Variables/_variables.scss";
 
+const TEST = gql`
+  query Query {
+    hello
+  }
+`;
+
 function App(): ReactJSXElement {
-  return (
+  const { loading, data } = useQuery(TEST);
+
+  return loading ? (
+    <p>Loading...</p> ) : 
+    data && (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          { data.hello }
         </p>
         <a
           className="App-link"
