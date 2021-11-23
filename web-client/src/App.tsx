fb784739220356c34ea016d523f2f8e67d80e2f1
Amanel <amanel.gauriat@gmail.com>
2021-11-23 11:43:08 +0100

    basic routes

--- web-client/src/App.tsx
+++ web-client/src/App.tsx
@@ -1,26 +1,22 @@
 import React from "react";
 import { ReactJSXElement } from "@emotion/react/types/jsx-namespace";
-import logo from "./logo.svg";
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
 
 function App(): ReactJSXElement {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        {/* <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route> */}
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </Router>
   );
 }
 
