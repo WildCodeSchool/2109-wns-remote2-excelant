a4579e6a8df4f97fc429afa6a3c6322cbe2f37d1
wild-e <wild.emmanuelle@gmail.com>
2021-11-23 11:35:34 +0100

    done for installing graphQL on client side + testing with hello query

--- web-client/src/App.tsx
+++ web-client/src/App.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
+import { gql, useQuery } from '@apollo/client';
 import logo from './logo.svg';
 import './App.css';
 
+const TEST = gql`
+  query Query {
+    hello
+  }
+`;
+
 function App() {
+  const { loading, data } = useQuery(TEST);
+  if (loading) return <p>Loading...</p>;
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          { data.hello }
         </p>
         <a
           className="App-link"
--- web-client/src/index.tsx
+++ web-client/src/index.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+} from "@apollo/client";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+  cache: new InMemoryCache()
+});
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ApolloProvider client={client}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  </ApolloProvider>,
   document.getElementById('root')
 );
 
