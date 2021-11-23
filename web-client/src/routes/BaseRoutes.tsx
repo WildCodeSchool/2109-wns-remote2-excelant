980108479335699befab5bcd9b7d9e9887e12865
Amanel <amanel.gauriat@gmail.com>
2021-11-23 15:06:33 +0100

    correct imports in  BaseRoutes file

--- web-client/src/routes/BaseRoutes.tsx
+++ web-client/src/routes/BaseRoutes.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "../pages/HomePage";
 import TasksPage from "../pages/TasksPage";
 import ProjectsPage from "../pages/ProjectsPage";
