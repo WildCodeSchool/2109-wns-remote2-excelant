84e9df544d387600c1985e0a0483ff21a2c1992e
Amanel <amanel.gauriat@gmail.com>
2021-11-23 15:02:02 +0100

    create a base navigation bar and routes

--- web-client/src/App.tsx
+++ web-client/src/App.tsx
@@ -1,23 +1,10 @@
 import React from "react";
 import { ReactJSXElement } from "@emotion/react/types/jsx-namespace";
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage";
+import BaseRoutes from "./routes/BaseRoutes";
 
 function App(): ReactJSXElement {
-  return (
-    <Router>
-      <Routes>
-        {/* <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/users">
-            <Users />
-          </Route> */}
-        <Route path="/" element={<HomePage />} />
-      </Routes>
-    </Router>
-  );
+  return <BaseRoutes />;
 }
 
 export default App;
---
+++ web-client/src/routes/BaseRoutes.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import HomePage from "../pages/HomePage";
+import TasksPage from "../pages/TasksPage";
+import ProjectsPage from "../pages/ProjectsPage";
+import UsersPage from "../pages/UsersPage";
+import NavBar from "../components/NavBar";
+
+const BaseRoutes: React.FC = () => (
+  <Router>
+    <Routes>
+      <Route path="/" element={<NavBar />}>
+        <Route index element={<HomePage />} />
+        <Route path="tasks" element={<TasksPage />} />
+        <Route path="projects" element={<ProjectsPage />} />
+        <Route path="users" element={<UsersPage />} />
+      </Route>
+    </Routes>
+  </Router>
+);
+
+export default BaseRoutes;
