a8331b1d29f70c578bb7c5fcfa01b2be843a0994
Amanel <amanel.gauriat@gmail.com>
2021-11-26 10:31:27 +0100

    in progress

--- web-client/src/components/tasks/TaskTable.tsx
+++ web-client/src/components/tasks/TaskTable.tsx
@@ -8,6 +8,7 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
+import { gql, useQuery } from "@apollo/client";
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
