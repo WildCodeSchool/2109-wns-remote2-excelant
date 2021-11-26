28f939bca42984676e7647160339cbf93e5743a2
Amanel <amanel.gauriat@gmail.com>
2021-11-26 14:58:48 +0100

    create task modal

--- web-client/src/pages/TasksPage.tsx
+++ web-client/src/pages/TasksPage.tsx
@@ -1,14 +1,23 @@
-import { Typography, Box } from "@mui/material";
-import React from "react";
+import { Typography, Box, Button } from "@mui/material";
+import React, { useState } from "react";
+import CreateTaskModal from "../components/tasks/CreateTaskModal";
 import TaskTable from "../components/tasks/TaskTable";
 
-const TasksPage: React.FC = () => (
-  <Box>
-    <Typography variant="h4" sx={{ textAlign: "center", mb: "16px" }}>
-      TasksPage
-    </Typography>
-    <TaskTable />
-  </Box>
-);
+const TasksPage: React.FC = () => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <Box>
+      <Typography variant="h4" sx={{ textAlign: "center", mb: "16px" }}>
+        TasksPage
+      </Typography>
+      <TaskTable />
+      <Button onClick={() => setOpen(true)} sx={{ ml: "300px" }}>
+        Create a new task
+      </Button>
+      <CreateTaskModal open={open} handleClose={() => setOpen(false)} />
+    </Box>
+  );
+};
 
 export default TasksPage;
