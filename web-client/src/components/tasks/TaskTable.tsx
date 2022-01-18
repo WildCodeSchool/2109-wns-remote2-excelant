8577de8873c9ec1e95a1c587ded641cb9642aeb4
Amanel Gauriat <69254106+AmanelGau@users.noreply.github.com>
2022-01-18 16:53:43 +0100

    Feature/task component (#26)
    * to be continued
    
    * done for making component table
    
    * in progress
    
    * in progress
    
    * show tasks with graphql
    
    * create task modal
    
    * allow refetch after creation of a task
    
    Co-authored-by: wild-e <wild.emmanuelle@gmail.com>
    Co-authored-by: Lionel <49095559+Lionel21@users.noreply.github.com>

---
+++ web-client/src/components/tasks/TaskTable.tsx
@@ -0,0 +1,94 @@
+import React, { useEffect } from "react";
+import { styled } from "@mui/material/styles";
+import {
+  Box,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+} from "@mui/material";
+import { tableCellClasses } from "@mui/material/TableCell";
+import { gql, useQuery } from "@apollo/client";
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  "&:nth-of-type(odd)": {
+    backgroundColor: theme.palette.action.hover,
+  },
+  // hide last border
+  "&:last-child td, &:last-child th": {
+    border: 0,
+  },
+}));
+
+const TaskTable: React.FC<{ reload: number }> = ({ reload }) => {
+  const TASKS_QUERY = gql`
+    query {
+      findAllTasks {
+        _id
+        name
+        project
+        status
+        assigne
+        dueDate
+      }
+    }
+  `;
+
+  const { loading, data, refetch } = useQuery(TASKS_QUERY);
+  console.log(data);
+
+  useEffect(() => {
+    if (reload > 0) {
+      refetch();
+    }
+  }, [reload]);
+
+  return loading ? (
+    <Box>Loading ... </Box>
+  ) : (
+    <TableContainer component={Paper} sx={{ pl: "250px", width: "100%" }}>
+      <Table aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <StyledTableCell>Subject</StyledTableCell>
+            <StyledTableCell align="right">Project</StyledTableCell>
+            <StyledTableCell align="right">Status</StyledTableCell>
+            <StyledTableCell align="right">Assignee</StyledTableCell>
+            <StyledTableCell align="right">Due date</StyledTableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {data &&
+            data.findAllTasks.map(
+              ({ _id, name, project, status, assigne, dueDate }: any) => (
+                <StyledTableRow key={_id}>
+                  <TableCell component="th" scope="row">
+                    {name}
+                  </TableCell>
+                  <TableCell align="right">{project}</TableCell>
+                  <TableCell align="right">{status}</TableCell>
+                  <TableCell align="right">{assigne}</TableCell>
+                  <TableCell align="right">{dueDate}</TableCell>
+                </StyledTableRow>
+              )
+            )}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+};
+
+export default TaskTable;
