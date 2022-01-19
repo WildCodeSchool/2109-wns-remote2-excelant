9f2975b1ddb740e7d5c99a5bc61e357761f0978c
Amanel Gauriat <69254106+AmanelGau@users.noreply.github.com>
2022-01-19 11:46:31 +0100

    fix apollo uri for docker (#36)
    Co-authored-by: Amanel Gauriat <a.gauriat@monkey-tie.com>

--- web-client/src/index.tsx
+++ web-client/src/index.tsx
@@ -6,8 +6,8 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 const client = new ApolloClient({
-  uri: "http://localhost:4000/graphql",
-  cache: new InMemoryCache(),
+  uri: "http://localhost:4040/graphql",
+  cache: new InMemoryCache()
 });
 
 ReactDOM.render(
