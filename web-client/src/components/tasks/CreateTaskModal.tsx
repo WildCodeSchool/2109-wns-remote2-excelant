334570f12120339fb57b6acfed07aaa21af76cd0
Amanel Gauriat <69254106+AmanelGau@users.noreply.github.com>
2022-01-19 18:37:10 +0100

    Change the modal design and add desciption box (#44)
    Co-authored-by: Amanel Gauriat <a.gauriat@monkey-tie.com>

--- web-client/src/components/tasks/CreateTaskModal.tsx
+++ web-client/src/components/tasks/CreateTaskModal.tsx
@@ -6,12 +6,14 @@ import {
   CardActions,
   Button,
   CircularProgress,
-  TextField
+  TextField,
+  Box
 } from "@mui/material";
 import React, { useState, useRef } from "react";
 import { FormProvider, useForm, NestedValue } from "react-hook-form";
 import { Formik, Form } from "formik";
 import { gql, useMutation } from "@apollo/client";
+import { modalStyle } from "../../_utils/modalStyle";
 
 interface CreateTaskInput {
   name: string;
@@ -61,8 +63,8 @@ const CreateTaskModal: React.FC<{ open: boolean; handleClose: () => void }> = ({
 
   return (
     <Modal open={open} onClose={handleClose}>
-      <Card>
-        <CardHeader title="Create a new task" />
+      <Card sx={{ ...modalStyle, padding: "8px 24px" }}>
+        <CardHeader title="Create a new task" sx={{ textAlign: "center" }} />
         <CardContent>
           {open && (
             <Formik
@@ -71,46 +73,101 @@ const CreateTaskModal: React.FC<{ open: boolean; handleClose: () => void }> = ({
             >
               {({ values, handleChange }) => (
                 <Form>
-                  <TextField
-                    name="name"
-                    value={values.name}
-                    onChange={handleChange}
-                  />
-                  <TextField
-                    name="project"
-                    value={values.project}
-                    onChange={handleChange}
-                  />
-                  <TextField
-                    name="status"
-                    value={values.status}
-                    onChange={handleChange}
-                  />
-                  <TextField
-                    name="assigne"
-                    value={values.assigne}
-                    onChange={handleChange}
-                  />
-                  <TextField
-                    name="dueDate"
-                    value={values.dueDate}
-                    onChange={handleChange}
-                  />
-                  <Button
-                    disabled={loading}
-                    variant="contained"
-                    onClick={() => onSubmit(values)}
-                  >
-                    Create
-                    {loading && (
-                      <CircularProgress
-                        style={{ width: 20, height: 20, marginLeft: "10px" }}
+                  <Box display="flex" flexDirection="column" gap={2}>
+                    <TextField
+                      name="name"
+                      value={values.name}
+                      onChange={handleChange}
+                      label="Name"
+                      size="small"
+                    />
+                    <Box
+                      display="flex"
+                      sx={{ flexDirection: { xs: "column", md: "row" } }}
+                      gap={1}
+                    >
+                      <TextField
+                        name="project"
+                        value={values.project}
+                        onChange={handleChange}
+                        label="Project"
+                        size="small"
+                        sx={{ flexGrow: 1 }}
                       />
-                    )}
-                  </Button>
-                  <Button disabled={loading} onClick={handleClose}>
-                    Annuler
-                  </Button>
+                      <TextField
+                        name="status"
+                        value={values.status}
+                        onChange={handleChange}
+                        label="Status"
+                        size="small"
+                        sx={{ flexGrow: 1 }}
+                      />
+                    </Box>
+                    <Box
+                      display="flex"
+                      sx={{ flexDirection: { xs: "column", md: "row" } }}
+                      gap={1}
+                    >
+                      <TextField
+                        name="assigne"
+                        value={values.assigne}
+                        onChange={handleChange}
+                        label="Assigne"
+                        size="small"
+                        sx={{ flexGrow: 1 }}
+                      />
+                      <TextField
+                        name="dueDate"
+                        value={values.dueDate}
+                        onChange={handleChange}
+                        label="Due Date"
+                        size="small"
+                        sx={{ flexGrow: 1 }}
+                      />
+                    </Box>
+                    {/**
+                     * The description field
+                     * Disabled because actually not supported
+                     */}
+                    <TextField
+                      name="description"
+                      value={""}
+                      onChange={handleChange}
+                      label="Description"
+                      size="small"
+                      multiline={true}
+                      minRows={5}
+                      disabled
+                    />
+                    <Box display="flex" justifyContent="space-evenly">
+                      <Button
+                        disabled={loading}
+                        variant="contained"
+                        onClick={() => onSubmit(values)}
+                        sx={{ width: "128px" }}
+                      >
+                        Create
+                        {loading && (
+                          <CircularProgress
+                            style={{
+                              width: 20,
+                              height: 20,
+                              marginLeft: "10px"
+                            }}
+                          />
+                        )}
+                      </Button>
+                      <Button
+                        variant="outlined"
+                        disabled={loading}
+                        onClick={handleClose}
+                        color="error"
+                        sx={{ width: "128px" }}
+                      >
+                        Cancel
+                      </Button>
+                    </Box>
+                  </Box>
                 </Form>
               )}
             </Formik>
