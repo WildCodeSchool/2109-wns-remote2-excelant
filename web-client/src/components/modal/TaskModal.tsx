535c5a20e42e43c8045452dc1441bb889d593005
Lionel Delamare <ldelamare@entreloups.com>
2022-01-21 11:55:21 +0100

    Modal stylization

--- web-client/src/components/modal/TaskModal.tsx
+++ web-client/src/components/modal/TaskModal.tsx
@@ -1,11 +1,10 @@
 import React from "react";
 
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
-import {Card, Grid, Stack} from "@mui/material";
-import { modalStyle } from "../../_utils/modalStyle";
-import {CardHeader} from "@mui/material";
+import {Card, Grid} from "@mui/material";
+import { taskModalStyle } from "../../_utils/modalStyle";
+
+import "./TaskModal.scss";
 
 const TaskModal: React.FC<{ open: boolean, task: any, handleClose: () => void }> = ({
     open,
@@ -14,20 +13,23 @@ const TaskModal: React.FC<{ open: boolean, task: any, handleClose: () => void }>
 }) => {
     return (
         <Modal open={open} onClose={handleClose}>
-            <Card sx={{ ...modalStyle, padding: "8px 24px" }}>
-                <Box display="flex" justifyContent="start">
-                    <Typography variant="h1" sx={{ textAlign: "center" }}>
-                        {task?.name}
-                    </Typography>
-                </Box>
-                <Box display="flex" justifyContent="end">
-                    <Typography variant="h2" sx={{ textAlign: "center" }}>Comments</Typography>
-                </Box>
-
-                {/*<p>{project}</p>*/}
-                {/*<p>{status}</p>*/}
-                {/*<p>{assigne}</p>*/}
-                {/*<p>{dueDate}</p>*/}
+            <Card sx={{ ...taskModalStyle, padding: "8px 24px" }}>
+                <Grid container>
+                    <Grid item xs={6}>
+                        <div className="modal__task_header">
+                            <h1 className="modal__task_first-title">{task?.name}</h1>
+                            <p className="modal__task_description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab alias architecto atque commodi corporis debitis deleniti deserunt dolorem doloremque eaque impedit, inventore laudantium libero magnam maxime nisi quo tempore voluptatibus.</p>
+                        </div>
+                        <div className="modal__task_informations">
+                            <p className="modal__task_informations-assigne">Assigne: </p> <span>{task?.assigne}</span> <br/>
+                            <p className="modal__task_informations-project">Project: </p> <span>{task?.project}</span> <br/>
+                            <p className="modal__task_informations-date">Due Date: </p> <span>{task?.dueDate}</span> <br/>
+                        </div>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <h2 className="modal__task_second-title">Comments</h2>
+                    </Grid>
+                </Grid>
             </Card>
         </Modal>
         );
