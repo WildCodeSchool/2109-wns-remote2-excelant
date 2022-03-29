3ae60fba4848e67c22ab4f329ce5d03937dfbe2c
Pierre Muller <pierremanu.muller@gmail.com>
2022-03-29 16:20:54 +0200

    feat(user): create User schema

---
+++ server/src/schema/User/user.schema.ts
@@ -0,0 +1,18 @@
+import { mongoose, prop } from '@typegoose/typegoose';
+import { Field, ID, ObjectType } from 'type-graphql';
+
+@ObjectType()
+class User {
+  @Field(() => ID)
+  readonly _id: mongoose.Schema.Types.ObjectId;
+
+  @Field(() => String)
+  @prop({ required: true, unique: true })
+  email: string;
+
+  @Field(() => String)
+  @prop({ required: true })
+  password: string;
+}
+
+export default User;
