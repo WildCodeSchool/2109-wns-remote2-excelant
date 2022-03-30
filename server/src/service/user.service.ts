ad74b78f5b8e6af3f3cb1c87b045fd1b4d808c33
Pierre Muller <pierremanu.muller@gmail.com>
2022-03-30 16:50:42 +0200

    feat(User): findone, update and delete
    Note: mutations for email and password are separate

--- server/src/service/user.service.ts
+++ server/src/service/user.service.ts
@@ -1,5 +1,8 @@
 import { UserModel } from '../schema';
 import CreateUserInput from '../schema/User/user.create';
+import FindOneUserInput from '../schema/User/user.find';
+import UpdateUserEmailInput from '../schema/User/user.updateEmail';
+import UpdateUserPasswordInput from '../schema/User/user.updatePassword';
 
 class UserService {
   // eslint-disable-next-line class-methods-use-this
@@ -9,8 +12,29 @@ class UserService {
   }
 
   // eslint-disable-next-line class-methods-use-this
+  async findOneUser(input: FindOneUserInput) {
+    const user = await UserModel.findOne(input);
+    return user;
+  }
+
+  // eslint-disable-next-line class-methods-use-this
   async createUser(input: CreateUserInput) {
-    return UserModel.create(input)
+    return UserModel.create(input);
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  async updateUserEmail(id: string, input: UpdateUserEmailInput) {
+    return UserModel.findByIdAndUpdate(id, input);
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  async updateUserPassword(id: string, input: UpdateUserPasswordInput) {
+    return UserModel.findByIdAndUpdate(id, input);
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  async deleteUser(id: string) {
+    return UserModel.findByIdAndDelete(id);
   }
 }
 
