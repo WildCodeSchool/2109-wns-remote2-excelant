bb1fcf22acc5cc8c9787186fb4f87069e43dee23
Lionel Delamare <ldelamare@entreloups.com>
2022-04-01 09:32:56 +0200

    Yup validation: register form

--- web-client/src/components/form/Register.tsx
+++ web-client/src/components/form/Register.tsx
@@ -1,34 +1,20 @@
 import React, { useState } from 'react';
 import { useMutation } from "@apollo/client";
 import GqlRequest from "../../_graphql/GqlRequest";
-import { Formik, Field, Form, ErrorMessage, useFormik } from "formik";
+import { useFormik } from "formik";
 import { registerSchema } from "../../yup/Register";
+import { Link, Navigate } from 'react-router-dom';
 
 import CssBaseline from '@mui/material/CssBaseline';
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
 import FormHelperText from '@mui/material/FormHelperText';
-import Alert from '@mui/material/Alert';
 import Button from '@mui/material/Button';
 import {CircularProgress} from "@mui/material";
 import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import ExcelantLogo from '../../images/logo_excelant.jpg';
-import { Link, Navigate } from 'react-router-dom';
-import CustomTextField from './TextField';
-
-interface CreateUserInput {
-    email: string;
-    password: string;
-    confirmPassword: string;
-}
-
-const registerDefaultValues: CreateUserInput = {
-    email: "",
-    password: "",
-    confirmPassword: "",
-}
 
 const Register = () => {
     const [toHome, setToHome] = useState(false);
@@ -36,30 +22,25 @@ const Register = () => {
     const [createUser] = useMutation(
         new GqlRequest("User").create("email, password, confirmPassword")
     );
-
-    const onSubmit = async (values: CreateUserInput) => {
-        setLoading(true);
-        try {
-           await createUser({ variables: { input: values }});
-        } catch (err) {
-           return console.log(`Error ${err}`);
-        } finally {
-            setLoading(false);
+    const formik = useFormik({
+        initialValues: {
+            email: "",
+            password: "",
+            confirmPassword: ""
+        },
+        validationSchema: registerSchema,
+        onSubmit: async (values) => {
+                setLoading(true);
+                try {
+                   await createUser({ variables: { input: values }});
+                } catch (err) {
+                   return console.log(`${err}`);
+                } finally {
+                    setLoading(false);
+                }
+            return setToHome(true);
         }
-
-        // if (enteredEmail.trim() === '') {
-        //     return setEnteredEmailIsValid(false);
-        // }
-        // setEnteredEmailIsValid(true);
-
-        // TODO: check password conditions
-        // setIsErrorConfirmPassword(e.target.value);
-        // if (password !== confirmPassword) return setIsErrorConfirmPassword("Confirm password should be match with password!");
-        // if (password.length < 6) return setIsErrorLengthPassword("Your password must contain at least 6 characters!");
-
-        console.log("Success form submission");
-        // return setToHome(true);
-    }
+    });
 
     return (
         <>
@@ -78,95 +59,65 @@ const Register = () => {
                     <Typography component="h2" variant="h5" sx={{ mt: 2, mb: 1 }}>
                         Sign Up
                     </Typography>
-                    <Formik
-                        initialValues={registerDefaultValues}
-                        validationSchema={registerSchema}
-                        onSubmit={(values) => onSubmit(values)}
-                    >
-                        {({ values, handleChange }) => (
-                            <Form>
-                                {/*<TextField*/}
-                                {/*    margin="normal"*/}
-                                {/*    required*/}
-                                {/*    fullWidth*/}
-                                {/*    id="email"*/}
-                                {/*    label="Email Address"*/}
-                                {/*    name="email"*/}
-                                {/*    type="email"*/}
-                                {/*    autoComplete="email"*/}
-                                {/*    autoFocus*/}
-                                {/*    value={values.email}*/}
-                                {/*    sx={{ width: "100%" }}*/}
-                                {/*    onChange={handleChange}*/}
-                                {/*/>*/}
-                                <CustomTextField
-                                    label="Email"
-                                    name="email"
-                                    type="email"
-                                />
-                                <ErrorMessage
-                                    name="email"
-                                    component="small"
-                                    className="text-danger"
-                                />
-                                <TextField
-                                    margin="normal"
-                                    required
-                                    fullWidth
-                                    id="password"
-                                    label="Password"
-                                    name="password"
-                                    type="password"
-                                    autoComplete="current-password"
-                                    value={values.password}
-                                    onChange={handleChange}
-                                />
-                                <ErrorMessage
-                                    name="password"
-                                    component="small"
-                                    className="text-danger"
-                                />
-                                <FormHelperText>
-                                    Your password must contain at least six characters, including a number, an upper case letter and a specific character
-                                </FormHelperText>
-                                <TextField
-                                    margin="normal"
-                                    required
-                                    fullWidth
-                                    id="confirmPassword"
-                                    label="Confirm Password"
-                                    name="confirmPassword"
-                                    type="password"
-                                    autoComplete="current-password"
-                                    value={values.confirmPassword}
-                                    onChange={handleChange}
-                                />
-                                <ErrorMessage
-                                    name="confirmPassword"
-                                    component="small"
-                                    className="text-danger"
-                                />
-                                <Button
-                                    disabled={loading}
-                                    fullWidth
-                                    variant="contained"
-                                    onClick={() => onSubmit(values)}
-                                    sx={{ mt: 3, mb: 2, bgcolor: "#B6B8EB", "&.MuiButtonBase-root:hover": { bgcolor: "#adafe0", boxShadow: 4 } }}
-                                >
-                                    Sign Up
-                                    {loading && (
-                                        <CircularProgress
-                                            style={{
-                                                width: 20,
-                                                height: 20,
-                                                marginLeft: "10px",
-                                            }}
-                                        />
-                                    )}
-                                </Button>
-                            </Form>
-                        )}
-                    </Formik>
+                        <form onSubmit={formik.handleSubmit}>
+                            <TextField
+                                fullWidth
+                                margin="normal"
+                                id="email"
+                                label="Email Address"
+                                name="email"
+                                type="email"
+                                value={formik.values.email}
+                                onChange={formik.handleChange}
+                                error={formik.touched.email && Boolean(formik.errors.email)}
+                                helperText={formik.touched.email && formik.errors.email}
+                            />
+                            <TextField
+                                fullWidth
+                                margin="normal"
+                                id="password"
+                                label="Password"
+                                name="password"
+                                type="password"
+                                value={formik.values.password}
+                                onChange={formik.handleChange}
+                                error={formik.touched.password && Boolean(formik.errors.password)}
+                                helperText={formik.touched.password && formik.errors.password}
+                            />
+                            <FormHelperText>
+                                Your password must contain at least six characters, including a number, an upper case letter and a specific character
+                            </FormHelperText>
+                            <TextField
+                                fullWidth
+                                margin="normal"
+                                id="confirmPassword"
+                                label="Confirm Password"
+                                name="confirmPassword"
+                                type="password"
+                                value={formik.values.confirmPassword}
+                                onChange={formik.handleChange}
+                                error={formik.touched.confirmPassword && Boolean(formik.errors.confirmPassword)}
+                                helperText={formik.touched.confirmPassword && formik.errors.confirmPassword}
+                            />
+                            <Button
+                                disabled={loading}
+                                fullWidth
+                                variant="contained"
+                                type="submit"
+                                sx={{ mt: 3, mb: 2, bgcolor: "#B6B8EB", "&.MuiButtonBase-root:hover": { bgcolor: "#adafe0", boxShadow: 4 } }}
+                            >
+                                Sign Up
+                                {loading && (
+                                    <CircularProgress
+                                        style={{
+                                            width: 20,
+                                            height: 20,
+                                            marginLeft: "10px",
+                                        }}
+                                    />
+                                )}
+                            </Button>
+                        </form>
                     <Grid container justifyContent="center">
                         <Grid item mt={1} mb={2}>
                             <Link to="/login" style={{ fontSize: "0.875rem" }}>
@@ -174,9 +125,9 @@ const Register = () => {
                             </Link>
                         </Grid>
                         <Grid item>
-                            {/*<Link  variant="body2">*/}
-                            {/*    Forgot password?*/}
-                            {/*</Link>*/}
+                            <Link to="#" style={{ fontSize: "0.875rem" }}>
+                                Forgot password?
+                            </Link>
                         </Grid>
                     </Grid>
                 </Box>
--- web-client/src/yup/Register.tsx
+++ web-client/src/yup/Register.tsx
@@ -5,11 +5,11 @@ export const registerSchema = Yup.object().shape({
         .email("Email is invalid")
         .required("Please insert an email address"),
     password: Yup.string()
-        .min(6, "Password must contain a least 6 characters")
+        .min(6, "Password must contain at least 6 characters")
         .max(25, "Password must not exceed 25 characters")
         .matches(
             new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})"),
-            "Password must contain one capital letter and a specific character"
+            "Password must contain at least one capital letter, one number and a specific character"
         )
         .required("Please enter a password"),
     confirmPassword: Yup.string()
