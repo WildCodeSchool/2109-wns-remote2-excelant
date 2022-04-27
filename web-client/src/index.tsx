498dc40cbc32c11bcb8a74b9e0af354dd9a47441
Amanel Gauriat <a.gauriat@monkey-tie.com>
2022-04-27 10:26:53 +0200

    cancel unwanted changes in index

--- web-client/src/index.tsx
+++ web-client/src/index.tsx
@@ -8,8 +8,6 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import Theme from "./theme/Theme";
 
-console.log(process.env.REACT_APP_DB_URI);
-
 const client = new ApolloClient({
   uri: process.env.REACT_APP_DB_URI,
   cache: new InMemoryCache(),
@@ -31,4 +29,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
+reportWebVitals();
