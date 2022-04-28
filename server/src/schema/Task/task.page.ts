388bfe4401e7e03fc0af6a65f10bd40dd5c04ecc
Lionel Delamare <ldelamare@entreloups.com>
2022-04-28 14:48:24 +0200

    task pagination

---
+++ server/src/schema/Task/task.page.ts
@@ -0,0 +1,37 @@
+import { Field, Int, ObjectType } from 'type-graphql';
+import Project from './task.schema';
+
+@ObjectType()
+class TaskPage {
+    @Field(() => [Task])
+    docs: [Task];
+
+    @Field(() => Int)
+    totalDocs: number;
+
+    @Field(() => Int)
+    limit: number;
+
+    @Field(() => Int)
+    totalPages: number;
+
+    @Field(() => Int)
+    page: number;
+
+    @Field(() => Int)
+    pagingCounter: number;
+
+    @Field(() => Int)
+    hasPrevPage: boolean;
+
+    @Field(() => Int)
+    hasNextPage: boolean;
+
+    @Field(() => String)
+    prevPage: string;
+
+    @Field(() => String)
+    nextPage: string;
+}
+
+export default TaskPage;
--- server/src/schema/Task/task.schema.ts
+++ server/src/schema/Task/task.schema.ts
@@ -1,8 +1,11 @@
-import { mongoose, prop, Ref } from '@typegoose/typegoose';
+import { mongoose, plugin, prop, Ref } from '@typegoose/typegoose';
 import { Field, ID, ObjectType } from 'type-graphql';
 import Project from '../Project/project.schema';
+import mongoosePaginate from 'mongoose-paginate-v2';
+import PaginateMethod from '../../utils/PaginateMethodType';
 
 @ObjectType()
+@plugin(mongoosePaginate)
 class Task {
   @Field(() => ID)
   readonly _id: mongoose.Schema.Types.ObjectId;
@@ -26,6 +29,8 @@ class Task {
   @Field()
   @prop({ required: true })
   dueDate: Date;
+
+  static paginate: PaginateMethod<Task>
 }
 
 export default Task;
