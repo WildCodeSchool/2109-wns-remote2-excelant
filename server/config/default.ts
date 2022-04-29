edd1acfb77d9ac225364f9a463a8957fcc1bbdef
Pierre Muller <61626102+kiw808@users.noreply.github.com>
2022-04-29 15:02:34 +0200

    [API] docker dev reload and env (#148)
    * chore(Dockerfile): server dev hot reload
    
    * fix(jwt): use key pair for authentication
    
    * refactor(server): use es6 syntax for dotenv
    
    * chore: update docker-compose prod and staging
    
    * chore: add nginx.conf
    
    * doc: update readme
    
    * fix(docker-compose): add REACT_APP_BACK_URI

--- server/config/default.ts
+++ server/config/default.ts
@@ -1,5 +1,3 @@
 export default {
   dbUri: 'mongodb://mongodb:27017/excelant',
-  publicKey: 'PUBLIC_KEY',
-  privateKey: 'PRIVATE_KEY',
 };
--- server/src/utils/jwt.ts
+++ server/src/utils/jwt.ts
@@ -1,24 +1,25 @@
-// import config from 'config';
+import config from 'config';
 import jwt from 'jsonwebtoken';
 
-// const publicKey = Buffer.from(
-//   config.get<string>('publicKey'),
-//   'base64'
-// ).toString('ascii');
-// const privateKey = Buffer.from(
-//   config.get<string>('privateKey'),
-//   'base64'
-// ).toString('ascii');
+const publicKey = Buffer.from(
+  config.get<string>('publicKey'),
+  'base64'
+).toString('ascii');
+const privateKey = Buffer.from(
+  config.get<string>('privateKey'),
+  'base64'
+).toString('ascii');
 
 export function signJwt(object: Object, options?: jwt.SignOptions) {
-  return jwt.sign(object, 'shhhhh', {
+  return jwt.sign(object, privateKey, {
     ...(options && options),
+    algorithm: 'RS256',
   });
 }
 
 export function verifyJwt<T>(token: string): T | null {
   try {
-    const decoded = jwt.verify(token, 'shhhhh') as T;
+    const decoded = jwt.verify(token, publicKey) as T;
     return decoded;
   } catch (err) {
     return null;
