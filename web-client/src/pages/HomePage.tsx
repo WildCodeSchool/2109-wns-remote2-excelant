963e5b335b09b3acbd223c906efb66ac2195fb08
Lionel Delamare <ldelamare@entreloups.com>
2022-06-22 17:18:49 +0200

    WIP

--- web-client/src/pages/HomePage.tsx
+++ web-client/src/pages/HomePage.tsx
@@ -1,13 +1,19 @@
 import React from "react";
-import { Container, Typography } from "@mui/material";
+import {Container, Typography} from "@mui/material";
+import UserContext from "../context/userContext";
+import {UserType} from "../_types/_userTypes";
 
-const HomePage: React.FC = () => (
-
-  <Container maxWidth="lg">
-    <Typography variant="h1" sx={{ backgroundColor: "primary.light" }}>
-      HomePage
-    </Typography>
-  </Container>
-);
+const HomePage: React.FC<{ user: UserType }> = ({user}) => {
+    console.log(user);
+    return (
+        <UserContext.Provider value={{user: user}}>
+            <Container maxWidth="lg">
+                <Typography variant="h1" sx={{backgroundColor: "primary.light"}}>
+                    HomePage
+                </Typography>
+            </Container>
+        </UserContext.Provider>
+    )
+};
 
 export default HomePage;
--- web-client/src/routes/BaseRoutes.tsx
+++ web-client/src/routes/BaseRoutes.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route, Location } from "react-router-dom";
+import React, { useContext } from "react";
+import {BrowserRouter as Router, Routes, Route, Location} from "react-router-dom";
 import HomePage from "../pages/HomePage";
 import TasksPage from "../pages/TasksPage";
 import ProjectsPage from "../pages/ProjectsPage";
@@ -7,22 +7,27 @@ import UsersPage from "../pages/UsersPage";
 import NavBar from "../components/navbar/NavBar";
 import Register from "../components/form/Register";
 import Login from "../components/form/Login";
+import UserContext from "../context/userContext";
 
-const BaseRoutes: React.FC = () => (
-  <>
-    <Router>
-      <Routes>
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/" element={<NavBar />}>
-          <Route index element={<HomePage />} />
-          <Route path="/tasks" element={<TasksPage />} />
-          <Route path="/projects" element={<ProjectsPage />} />
-          <Route path="/users" element={<UsersPage />} />
-        </Route>
-      </Routes>
-    </Router>
-  </>
-);
+const BaseRoutes: React.FC = () => {
+    const { user } = useContext(UserContext)
+
+    return (
+        <>
+            <Router>
+                <Routes>
+                    <Route path="/register" element={<Register/>}/>
+                    <Route path="/login" element={<Login/>}/>
+                    <Route path="/" element={<NavBar/>}>
+                        <Route index element={<HomePage user={user}/>}/>
+                        <Route path="/tasks" element={<TasksPage/>}/>
+                        <Route path="/projects" element={<ProjectsPage/>}/>
+                        <Route path="/users" element={<UsersPage/>}/>
+                    </Route>
+                </Routes>
+            </Router>
+        </>
+    )
+};
 
 export default BaseRoutes;
