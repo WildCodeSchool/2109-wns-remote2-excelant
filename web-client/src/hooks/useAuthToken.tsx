0fbf132cd615b3fb75b2d3d22eb8b533929ba361
Pierre Muller <pierremanu.muller@gmail.com>
2022-06-29 16:29:22 +0200

    feat(apolloClient): credentials in HttpLink

--- web-client/src/hooks/useAuthToken.tsx
+++ web-client/src/hooks/useAuthToken.tsx
@@ -3,17 +3,19 @@ import { useCookies } from "react-cookie";
 const TOKEN_NAME = "accessToken";
 
 // Custom hook to handle authToken - we use compositon to decouple the auth system and it's storage
-export const useAuthToken = () => {
-    // We use react-cookies to access our cookies
-    const [cookies, setCookie, removeCookie] = useCookies([TOKEN_NAME]);
+const useAuthToken = () => {
+  // We use react-cookies to access our cookies
+  const [cookies, setCookie, removeCookie] = useCookies([TOKEN_NAME]);
 
-    // This function allows to save any string in our cookies, under the key "authToken"
-    const setAuthToken = (authToken: string) => {
-        setCookie(TOKEN_NAME, authToken);
-    };
+  // This function allows to save any string in our cookies, under the key "authToken"
+  const setAuthToken = (authToken: string) => {
+    setCookie(TOKEN_NAME, authToken);
+  };
 
-    // This function removes the key "authToken" from our cookies. Useful to logout
-    const removeAuthToken = () => removeCookie(TOKEN_NAME);
+  // This function removes the key "authToken" from our cookies. Useful to logout
+  const removeAuthToken = () => removeCookie(TOKEN_NAME);
 
-    return [cookies[TOKEN_NAME], setAuthToken, removeAuthToken];
-}
+  return [cookies[TOKEN_NAME], setAuthToken, removeAuthToken];
+};
+
+export default useAuthToken;
