3ff794d8767f78ee9341c60b3c354f73196aed31
Pierre Muller <pierremanu.muller@gmail.com>
2022-06-30 14:09:19 +0200

    feat: set jwt in localstorage at login mutation
    also add roles to _userTypes

--- web-client/src/hooks/loginMutation.ts
+++ web-client/src/hooks/loginMutation.ts
@@ -2,7 +2,15 @@ import { gql, FetchResult, MutationResult, useMutation } from "@apollo/client";
 
 export const loginMutationGQL = gql`
   mutation login($email: String!, $password: String!) {
-    login(input: { email: $email, password: $password })
+    login(input: { email: $email, password: $password }) {
+      user {
+        _id
+        name
+        email
+        roles
+      }
+      accessToken
+    }
   }
 `;
 
@@ -10,7 +18,12 @@ export const useLoginMutation: <TData>() => [
   (email: string, password: string) => Promise<FetchResult<TData>>,
   MutationResult<TData>
 ] = () => {
-  const [mutation, mutationResults] = useMutation(loginMutationGQL);
+  const [mutation, mutationResults] = useMutation(loginMutationGQL, {
+    onCompleted: (data) => {
+      const { login } = data;
+      localStorage.setItem("accessToken", login.accessToken);
+    },
+  });
 
   // We have rewritten the function to have a cleaner interface
   const login = (email: string, password: string) =>
